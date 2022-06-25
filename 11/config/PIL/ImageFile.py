12fb8644d012a15f99055daf7e584015543c6edb
Nawal Singh <nawalsingh666@gmail.com>
2022-06-25 08:14:58 +0000

    next batch config

---
+++ 11/config/PIL/ImageFile.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cae1164ab7e09c545a3601c4c945749a0a2b4d1f369fbe27483b1d54fc1a2d74
+size 22701
---
+++ 11/config/PIL/_version.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:186043b7451e593ceba2029f8eec52d8d01d32e5b47b04e978ce793fc01d38ba
+size 50
