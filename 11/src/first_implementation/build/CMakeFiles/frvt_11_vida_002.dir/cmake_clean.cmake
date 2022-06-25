095fc0f0f12e97c4d9a89517df18a515cc095f79
Nawal Singh <nawalsingh666@gmail.com>
2022-06-25 06:00:58 +0000

    added src

---
+++ 11/src/first_implementation/build/CMakeFiles/frvt_11_vida_002.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ubuntu/frvt/11/lib/libfrvt_11_vida_002.pdb"
+  "/home/ubuntu/frvt/11/lib/libfrvt_11_vida_002.so"
+  "CMakeFiles/frvt_11_vida_002.dir/firstimplfrvt11.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/frvt_11_vida_002.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
